50fb182e48c0ae8edf46917050c69b76ac9c484c
bluesyy <vndtjsrl3@gmail.com>
2023-04-16 12:13:10 +0900

    #21 - 검색기능 추가
    JPA 기능을 이용해서 간단하게 검색기능을 추가하였다.
    
    * 수정시 이미지 첨부안하면 null 오류 뜨는데 나중에 해결하기로 함..

--- src/main/java/com/study/board/repository/BoardRepository.java
+++ src/main/java/com/study/board/repository/BoardRepository.java
@@ -1,9 +1,17 @@
 package com.study.board.repository;
 
 import com.study.board.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Integer> {
+
+    // JPA 를 이용해서 검색기능을 추가해보자.
+    // 명명규칙을 잘 지켜야함 -> 대문자 소문자
+    Page<Board> findByTitleContaining(String searchKeyword, Pageable pageable);
+
+    
 }
